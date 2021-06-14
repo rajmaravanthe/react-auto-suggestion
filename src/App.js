e169e4ee45581782cfbdad67676cc6c953d6ca02
rajmaravanthe <rraajjpoojary@gmail.com>
2021-06-14 20:21:41 +0530

    added logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+const searchedData = [
+  "rajesh",
+  "santhosh",
+  "mahesh",
+  "girish",
+  "anish",
+  "ravish",
+  "vinish",
+];
 
 function App() {
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+
+  const manipulateSuggestions = (event) => {
+    setValue(event.target.value);
+    setSuggestions(
+      searchedData.map((suggestion, i) => {
+        if (suggestion.indexOf(event.target.value) > -1) {
+          return (
+            <li
+              key={i}
+              onClick={(event) => {
+                setValue(suggestion);
+              }}
+            >
+              {suggestion}
+            </li>
+          );
+        }
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <input
+        type='text'
+        value={value}
+        onChange={(event) => manipulateSuggestions(event)}
+      />
+      <div>
+        <ul>{suggestions}</ul>
+      </div>
     </div>
   );
 }
